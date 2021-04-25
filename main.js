109ba09463fe6d4b9e3bfd6281e449a7cbeff20e
Aayush Pal <ap.aayushpal@gmail.com>
2021-04-25 16:12:49 +0530

    Ok capturing the action from each citations

--- main.js
+++ main.js
@@ -2,16 +2,68 @@ window.addEventListener("load", () => {
   for (let cites of document.querySelectorAll(".cit")) {
     cites.addEventListener("click", () => {
       switch (cites.id) {
-        case cites.id == 1:
+        case "1":
           console.log("1");
           break;
-        case cites.id == 2:
+        case "2":
           console.log("2");
           break;
-        case cites.id == 3:
+        case "3":
           console.log("3");
           break;
+        case "4":
+          console.log("4");
+          break;
+        case "5":
+          console.log("5");
+          break;
+        case "6":
+          console.log("6");
+          break;
+        case "7":
+          console.log("7");
+          break;
+        case "8":
+          console.log("8");
+          break;
+        case "9":
+          console.log("9");
+          break;
+        case "10":
+          console.log("10");
+          break;
+        case "11":
+          console.log("10");
+          break;
+        case "12":
+          console.log("10");
+          break;
+        case "13":
+          console.log("10");
+          break;
+        case "14":
+          console.log("10");
+          break;
+        case "15":
+          console.log("10");
+          break;
+        case "16":
+          console.log("10");
+          break;
+        case "17":
+          console.log("10");
+          break;
+        case "18":
+          console.log("10");
+          break;
+        case "19":
+          console.log("10");
+          break;
+        case "20":
+          console.log("10");
+          break;
         default:
+          console.log("sd");
           break;
       }
     });
