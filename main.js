0bda05c015020a065e4d17b330640f4bf03318b2
Aayush Pal <ap.aayushpal@gmail.com>
2021-04-29 15:39:47 +0530

    Major changes:
    - Fixed couple of sizing issues
    - The video is now the main focus
    - Left pane appears on scroll > 800

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const sharebtn = document.querySelector("#share-custom-btn");
+const leftpane = document.querySelector(".left");
 
 const shareData = {
   url: "https://frosty-hoover-71d955.netlify.app/",
 };
 
 window.addEventListener("load", () => {
+  // leftpane.style.display = "none";
   if (!navigator.share) {
     sharebtn.style.display = "none";
   }
@@ -13,3 +15,15 @@ window.addEventListener("load", () => {
 sharebtn.addEventListener("click", async () => {
   navigator.share(shareData);
 });
+
+let scrollFunc = () => {
+  let y = window.scrollY;
+
+  if (y > 800 && screen.width > 1440) {
+    leftpane.style.display = "block";
+  } else {
+    leftpane.style.display = "none";
+  }
+};
+
+document.addEventListener("scroll", scrollFunc);
