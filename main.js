76704b039a2a031effe4c4d2c38d2013fbd7c3c5
Aayush Pal <ap.aayushpal@gmail.com>
2021-04-29 18:01:09 +0530

    Added a fabbar for the mobile screens

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const sharebtn = document.querySelector("#share-custom-btn");
 const leftpane = document.querySelector(".left");
+const contactbtn = document.querySelector("#contact-btn");
+const bottomsheet = document.querySelector("#bottomsheet");
+const closebtn = document.querySelector("#close-btn");
+const sharefabbtn = document.querySelector("#share-btn");
 
 const shareData = {
   url: "https://frosty-hoover-71d955.netlify.app/",
@@ -9,6 +13,8 @@ window.addEventListener("load", () => {
   // leftpane.style.display = "none";
   if (!navigator.share) {
     sharebtn.style.display = "none";
+    sharefabbtn.style.display = "none";
+    contactbtn.style.width = "100%";
   }
 });
 
@@ -16,10 +22,33 @@ sharebtn.addEventListener("click", async () => {
   navigator.share(shareData);
 });
 
+sharefabbtn.addEventListener("click", async () => {
+  navigator.share(shareData);
+});
+
+function hideBottomsheet() {
+  bottomsheet.classList.add("bottomsheet-hide-div");
+  bottomsheet.classList.remove("bottomsheet-summon-div");
+  setTimeout(() => {
+    bottomsheet.style.display = "none";
+  }, 500);
+}
+
+contactbtn.addEventListener("click", (e) => {
+  bottomsheet.classList.add("bottomsheet-summon-div");
+  bottomsheet.classList.remove("bottomsheet-hide-div");
+  bottomsheet.style.display = "block";
+  e.stopPropagation();
+});
+
+closebtn.addEventListener("click", (e) => {
+  hideBottomsheet();
+});
+
 let scrollFunc = () => {
   let y = window.scrollY;
 
-  if (y > 800 && screen.width > 1440) {
+  if (y > 700 && screen.width > 1440) {
     leftpane.style.display = "block";
   } else {
     leftpane.style.display = "none";
